2c4f167f340da395c0be7977082aaf145c6dba02
Gerald Bauer <gerald.bauer@gmail.com>
2021-05-12 14:08:22 +0200

    up

--- cryptopunks/lib/cryptopunks/composite.rb
+++ cryptopunks/lib/cryptopunks/composite.rb
@@ -2,51 +2,56 @@ module Cryptopunks
   class Image
   class Composite    ## nest Composite inside Image - why? why not?
 
+    PUNK_HASH   = 'ac39af4793119ee46bbff351d8cb6b5f23da60222126add4268e261199a2921b'
 
-    def self.read( path='./punks.png' )
-      data = File.open( path, 'rb' ) { |f| f.read }
-      new( data )
+    def self.sha256( data )
+      ## todo/check: or just use Digest::SHA256.hexdigest - why? why not?
+      Digest::SHA256.digest( data ).unpack( 'H*' )[0]
     end
 
 
     PUNK_HEIGHT = 24
     PUNK_WIDTH  = 24
 
-    PUNK_HASH   = 'ac39af4793119ee46bbff351d8cb6b5f23da60222126add4268e261199a2921b'
+    def self.read( path='./punks.png' )
+      data = File.open( path, 'rb' ) { |f| f.read }
 
-
-    def initialize( data )
-      @punks = ChunkyPNG::Image.from_blob( data )
-      puts "     #{@punks.height}x#{@punks.width} (height x width)"
-
-      @punk_rows  = @punks.width  / PUNK_WIDTH    ## e.g. 2400/24 = 100
-      @punk_cols  = @punks.height / PUNK_HEIGHT   ## e.g. 2400/24 = 100
-      @punk_count = @punk_rows * @punk_cols       ## ## 10000 = 100x100  (2400x2400 pixel)
-
-      ## check sha256 checksum
-      @hexdigest = sha256( data )
-      if original?
-         puts "     >#{@hexdigest}< SHA256 hash matching"
+      hexdigest = sha256( data )   ## check sha256 checksum
+      if hexdigest == PUNK_HASH
+         puts "     >#{hexdigest}< SHA256 hash matching"
          puts "         ✓ True Official Genuine CryptoPunks™ verified"
+      else
+         puts "         ✓ True Official Genuine Yes, You Can! Punks Not Dead™ verified"
       end
+
+      img = ChunkyPNG::Image.from_blob( data )
+      new( nil, nil, img )
     end
 
 
-    def sha256( data )
-      ## todo/check: or just use Digest::SHA256.hexdigest - why? why not?
-      Digest::SHA256.digest( data ).unpack( 'H*' )[0]
+    def initialize( cols=3, rows=3, initial=nil )
+      ## todo/check - find a better name for cols/rows - why? why not?
+      if initial
+        ## pass image through as-is
+        @punks = initial
+
+        @punk_rows  = @punks.width  / PUNK_WIDTH    ## e.g. 2400/24 = 100
+        @punk_cols  = @punks.height / PUNK_HEIGHT   ## e.g. 2400/24 = 100
+        @punk_count = @punk_rows * @punk_cols       ## ## 10000 = 100x100  (2400x2400 pixel)
+      else
+        @punk_rows = rows
+        @punk_cols = cols
+        @punks = ChunkyPNG::Image.new( @punk_cols * PUNK_WIDTH,
+                                       @punk_rows * PUNK_HEIGHT )
+        @punk_count = 0   # (track) current index (of added images)
+      end
+
+      puts "     #{@punks.height}x#{@punks.width} (height x width)"
     end
 
 
-    def hexdigest()  @hexdigest end
-
-    def verify?()  @hexdigest == PUNK_HASH; end
-    alias_method :genuine?,  :verify?
-    alias_method :original?, :verify?
-
-
-
-    def size() @punk_count; end
+    def count() @punk_count; end
+    alias_method :size, :count   ## add size alias (confusing if starting with 0?) - why? why not?
 
 
     def punk( index )
@@ -57,12 +62,35 @@ module Cryptopunks
     alias_method :[], :punk
 
 
+    def add( image )
+      y, x =  @punk_count.divmod( @punk_cols )
+
+      puts "    [#{@punk_count}] @ (#{x}/#{y}) #{image.width}x#{image.height} (height x width)"
+
+      ## note: image.image  - "unwrap" the "raw" ChunkyPNG::Image
+      @punks.compose!( image.image, x*PUNK_WIDTH, y*PUNK_HEIGHT )
+      @punk_count += 1
+    end
+    alias_method :<<, :add
+
+
+
     ####
     # more image helpers / delegates
     def width()   @punks.width; end
     def height()  @punks.height; end
 
     def pixels()  @punks.pixels; end
+
+    def save( path, constraints = {} )
+       # step 1: make sure outdir exits
+       outdir = File.dirname( path )
+       FileUtils.mkdir_p( outdir )  unless Dir.exist?( outdir )
+
+       # step 2: save
+       @punks.save( path, constraints )
+    end
+    alias_method :write, :save
 
   end ## class Composite
   end ## class Image
---
+++ morepunks/morepunks.rb
@@ -0,0 +1,28 @@
+require 'cryptopunks'
+
+
+rootdir = "../../programming-cryptopunks/i"
+
+## 25x40 = 1000 punks (cols x rows); create an empty composite
+morepunks = Punks::Image::Composite.new( 25, 40 )
+
+## read in 10 packs of 100 each
+(1..10).each do |series|
+  punks = Punks::Image::Composite.read( "#{rootdir}/more-punks-#{series}.png" )
+
+  ## for testing save first punk in series
+  punk = punks[0]
+  punk.zoom(4).save( "./tmp/morepunk-#{10000+(100*(series-1))}.png" )
+
+  ## add 100 more to morepunks composite
+  (0..99).each do |num|
+    morepunks << punks[num]
+  end
+end
+
+
+morepunks.save( "./tmp/morepunks.png" )
+
+
+puts "bye"
+
