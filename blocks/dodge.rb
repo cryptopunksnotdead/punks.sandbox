35e998f23632e34daf86e2963bc257542eca83ca
Gerald Bauer <gerald.bauer@gmail.com>
2021-08-06 13:34:40 +0200

    up

--- blocks/dodge.rb
+++ blocks/dodge.rb
@@ -7,8 +7,12 @@ $LOAD_PATH.unshift( "../cryptopunks/lib" )
 require 'cryptopunks'
 
 
+
 ## note: 32x32 pixel matric
-dodge = Image.parse( <<TXT, colors: ['000000', 'DB8A37', 'EDC685', 'FFFFFF' ] )
+types = {
+ dodge: {
+   colors: ['000000', 'DB8A37', 'EDC685', 'FFFFFF' ],
+   design: <<TXT,
 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
@@ -42,14 +46,10 @@ dodge = Image.parse( <<TXT, colors: ['000000', 'DB8A37', 'EDC685', 'FFFFFF' ] )
 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
 TXT
-
-dodge.save( "./tmp/dodge.png" )
-dodge.zoom(4).save( "./tmp/dodge4x.png" )
-dodge.zoom(8).save( "./tmp/dodge8x.png" )
-
-
-
-dodge = Image.parse( <<TXT, colors: ['000000', '4D6341', '5E7253', '7DA269', '9BBC88', 'FF0000' ] )
+ },
+ "dodge-zombie": {
+  colors: ['000000', '4D6341', '5E7253', '7DA269', '9BBC88', 'FF0000'],
+  design: <<TXT,
 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
@@ -83,13 +83,10 @@ dodge = Image.parse( <<TXT, colors: ['000000', '4D6341', '5E7253', '7DA269', '9B
 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
 TXT
-
-dodge.save( "./tmp/dodge-zombie.png" )
-dodge.zoom(4).save( "./tmp/dodge-zombie4x.png" )
-dodge.zoom(8).save( "./tmp/dodge-zombie8x.png" )
-
-
-dodge = Image.parse( <<TXT, colors: ['000000', '9BE0E0', 'C8FBFB' ] )
+ },
+ "dodge-alien": {
+  colors: ['000000', '9BE0E0', 'C8FBFB' ],
+  design: <<TXT,
 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
@@ -123,13 +120,10 @@ dodge = Image.parse( <<TXT, colors: ['000000', '9BE0E0', 'C8FBFB' ] )
 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
 TXT
-
-dodge.save( "./tmp/dodge-alien.png" )
-dodge.zoom(4).save( "./tmp/dodge-alien4x.png" )
-dodge.zoom(8).save( "./tmp/dodge-alien8x.png" )
-
-
-dodge = Image.parse( <<TXT, colors: [ '000000', '562600', '723709', '713F1D', '96512B'] )
+},
+"dodge-dark": {
+  colors: [ '000000', '562600', '723709', '713F1D', '96512B'],
+  design: <<TXT,
 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
@@ -163,9 +157,112 @@ dodge = Image.parse( <<TXT, colors: [ '000000', '562600', '723709', '713F1D', '9
 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
 TXT
-dodge.save( "./tmp/dodge-dark.png" )
-dodge.zoom(4).save( "./tmp/dodge-dark4x.png" )
-dodge.zoom(8).save( "./tmp/dodge-dark8x.png" )
+}
+}
+
+
+def cutoff_top( img )   ## "wipe-out" pixel lines 7,8,9 (top)
+  [7,8,9].each do |y|
+    img.width.times do |x|
+      img[ x, y ] = Color::TRANSPARENT
+    end
+  end
+  img
+end
+
+
+
+dodge = nil
+types.each do |key,type|
+   dodge = Image.parse( type[:design], colors: type[:colors] )
+   dodge.save( "./tmp/#{key}.png")
+   dodge.zoom(4).save( "./tmp/#{key}4x.png" )
+   dodge.zoom(8).save( "./tmp/#{key}8x.png" )
+
+   dodge = cutoff_top( dodge )
+   dodge.save( "./tmp/#{key}_notop.png" )   ## check: change notop to noears or nohead or such or ???
+end
+
+
+beanie = Image.parse( <<TXT, colors: ['000000', '0060C3', 'D60404', '3CC300', 'E4EB17'] )
+. . . . x x x x x . . . .
+. . . . . . @ . . . . . .
+. . x x x ~ ~ ~ o o o . .
+. x x x x ~ ~ ~ o o o o .
+x x x x ~ ~ ~ ~ ~ o o o o
+x x x ~ ~ ~ ~ ~ ~ ~ o o o
+. . . ^ ^ ^ ^ ^ ^ ^ . . .
+TXT
+
+dodge = Image.new( 32, 32 )
+dodge.compose!( beanie, 11, 6 )
+dodge.save( "./tmp/beanie-dodge.png" )
+dodge.zoom(4).save( "./tmp/beanie-dodgex4.png" )
+dodge.zoom(8).save( "./tmp/beanie-dodgex8.png" )
+
+
+
+_3dglasses = Image.parse( <<TXT, colors: ['328DFD', 'FD3232', 'F0F0F0'] )
+o o o o o o o o o o o o o o o
+. o o @ @ @ @ @ o x x x x x o
+. . o @ @ @ @ @ o x x x x x o
+. . o o o o o o o o o o o o o
+TXT
+
+dodge = Image.new( 32, 32 )
+dodge.compose!( _3dglasses, 10, 13 )
+dodge.save( "./tmp/3dglasses-dodge.png" )
+dodge.zoom(4).save( "./tmp/3dglasses-dodgex4.png" )
+dodge.zoom(8).save( "./tmp/3dglasses-dodgex8.png" )
+
+
+
+
+headband = Image.parse( <<TXT, colors: ['1A6ED5','FFFFFF'] )
+x x x x x x x x x x x
+@ @ @ @ @ @ @ @ @ @ @
+TXT
+
+dodge = Image.new( 32, 32 )
+dodge.compose!( headband, 12, 11 )
+dodge.save( "./tmp/headband-dodge.png" )
+dodge.zoom(4).save( "./tmp/headband-dodgex4.png" )
+dodge.zoom(8).save( "./tmp/headband-dodgex8.png" )
+
+
+
+knitted_cap = Image.parse( <<TXT, colors: ['000000', '933709', 'CA4E11'] )
+. . . @ @ @ @ @ @ @ @ @ @ @ . . .
+. . @ o o o o o o o o o o o @ . .
+. @ o o o o o o o o o o o o o @ .
+@ x x x x x x x x x x x x x x x @
+@ x o x o x o x o x o x o x o x @
+TXT
+
+dodge = Image.new( 32, 32 )
+dodge.compose!( knitted_cap, 9, 8 )
+dodge.save( "./tmp/knittedcap-dodge.png" )
+dodge.zoom(4).save( "./tmp/knittedcap-dodgex4.png" )
+dodge.zoom(8).save( "./tmp/knittedcap-dodgex8.png" )
+
+
+
+
+cap = Image.parse( <<TXT, colors: [ '8119B7', 'B261DC'] )
+. . @ @ @ @ @ @ @ @ @ @ @ . . . . . .
+. @ @ @ @ @ @ @ @ @ x @ @ @ . . . . .
+@ @ @ @ @ @ @ @ @ @ @ x @ @ . . . . .
+@ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ . .
+@ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @
+TXT
+
+dodge = Image.new( 32, 32 )
+dodge.compose!( cap, 10, 8 )
+dodge.save( "./tmp/cap-dodge.png" )
+dodge.zoom(4).save( "./tmp/cap-dodgex4.png" )
+dodge.zoom(8).save( "./tmp/cap-dodgex8.png" )
+
+
 
 
 puts "bye"
