872ee1220b7b928a25c3dbd7ac9a471ec49ece16
Gerald Bauer <gerald.bauer@gmail.com>
2021-08-10 13:15:03 +0200

    up

--- blocks/attributes.rb
+++ blocks/attributes.rb
@@ -10,6 +10,163 @@ require 'cryptopunks'
 
 attributes = {
 
+
+"wallstreetbets-hair": {
+  colors: ['000000', 'E8BC0D'],
+  offset: [3,2],
+  design: <<TXT,
+  . . . . @ @ @ @ @ @ @ @ @ @
+  . . . . x x x x x x x x x @
+  . . . @ @ @ @ @ @ @ @ @ x @
+  . . x x x x x x x x x @ x @
+  . @ @ @ @ @ @ @ @ @ x @ x @
+  . x x x x x x x x @ x @ x @
+  . @ @ @ @ @ . . . . . . x @
+  x x x x x . . . . . . . . .
+  @ @ @ @ . . . . . . . . . .
+  . x x . . . . . . . . . . .
+  . @ @ . . . . . . . . . . .
+TXT
+},
+
+eyepatch: {
+  colors: ['000000'],
+  offset: [6,10],
+  design: <<TXT,
+ @ @ @ @ @ @ @ @ @ @ @
+ . . @ @ @ @ . . . . .
+ . . @ @ @ @ . . . . .
+ . . . @ @ . . . . . .
+TXT
+},
+
+bigshades: {
+  colors: ['000000', '690C45', '8C0D5B', 'AD2160'],
+  offset: [6,9],
+  design: <<TXT,
+ . @ @ @ @ @ . @ @ @ @ @
+ . @ x x x @ @ @ x x x @
+ @ @ o o o @ . @ o o o @
+ . @ ^ ^ ^ @ . @ ^ ^ ^ @
+ . . @ @ @ . . . @ @ @ .
+TXT
+},
+
+nerdglasses: {
+  colors: ['000000', '80DBDA'],
+  offset: [6,10],
+  design: <<TXT,
+ . . @ @ @ @ . @ @ @ @
+ @ @ @ x x @ @ @ x x @
+ . . @ x x @ . @ x x @
+ . . @ @ @ @ . @ @ @ @
+TXT
+},
+
+regularshades: {
+  colors: ['000000'],
+  offset: [5,11],
+  design: <<TXT,
+ @ @ @ @ @ @ @ @ @ @ @ @ @
+ . . . @ @ @ @ . . @ @ @ @
+ . . . . @ @ . . . . @ @ .
+TXT
+},
+
+classicshades: {
+  colors: ['000000', '5C390F', 'C77514'],
+  offset: [6,10],
+  design: <<TXT,
+@ @ @ @ @ @ @ @ @ @ @
+. . @ x x @ . @ x x @
+. . @ o o @ . @ o o @
+. . . @ @ . . . @ @ .
+TXT
+},
+
+"3dglasses": {
+  colors: ['328DFD', 'FD3232', 'F0F0F0'],
+  offset: [6,10],
+  design: <<TXT,
+o o o o o o o o o o o
+. o o @ @ @ o x x x o
+. . o @ @ @ o x x x o
+. . o o o o o o o o o
+TXT
+},
+
+bandana: {
+  colors: ['142C7C', '1637A4', '1A43C8'],
+  offset: [2,5],
+  design: <<TXT,
+. . . . . . x x x x x x x x .
+. . . . . x o o o o o o o o x
+. . . . x o o o o o o o o o x
+o x o x @ o x x x x o o o x .
+. o x @ . . . . . . x x x . .
+. o @ . . . . . . . . . . . .
+. o . . . . . . . . . . . . .
+TXT
+},
+
+fedora: {
+  colors: ['3D2F1E', '000000'],
+  offset: [3,3],
+  design: <<TXT,
+ . . . . . . @ @ @ @ @ . . . . . .
+ . . . . . @ @ @ @ @ @ @ . . . . .
+ . . . . . @ @ @ @ @ @ @ . . . . .
+ . . . . @ @ @ @ @ @ @ @ @ . . . .
+ . . . x x x x x x x x x x x . . .
+ . @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ .
+ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @
+TXT
+},
+
+cowboyhat: {
+  colors: ['794B11', '502F05'],
+  offset: [2,3],
+  design: <<TXT,
+ . . . . . . @ @ . . . @ @ . . . . . .
+ . . . . . @ @ @ @ @ @ @ @ @ . . . . .
+ . . . . . @ @ @ @ @ @ @ @ @ . . . . .
+ . . . . . @ @ @ @ @ @ @ @ @ . . . . .
+ @ . . . x x x x x x x x x x x . . . @
+ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @
+ . @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ .
+TXT
+},
+
+tophat: {
+  colors: ['000000', 'DC1D1D'],
+  offset: [4,1],
+  design: <<TXT,
+ . . . @ @ @ @ @ @ @ @ @ . . .
+ . . @ @ @ @ @ @ @ @ @ @ @ . .
+ . . @ @ @ @ @ @ @ @ @ @ @ . .
+ . . @ @ @ @ @ @ @ @ @ @ @ . .
+ . . @ @ @ @ @ @ @ @ @ @ @ . .
+ . . x x x x x x x x x x x . .
+ . @ @ @ @ @ @ @ @ @ @ @ @ @ .
+ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @
+TXT
+},
+
+beanie: {
+  colors: ['000000', '0060C3', 'D60404', 'E4EB17', '3CC300'],
+  offset: [6,3],
+  design: <<TXT,
+  . . . x x x x x . . .
+  . . . . . @ . . . . .
+  . . x x ^ ^ ^ o o . .
+  . x x x ^ ^ ^ o o o .
+  x x x ^ ^ ^ ^ ^ o o o
+  x x ^ ^ ^ ^ ^ ^ ^ o o
+  . . ~ ~ ~ ~ ~ ~ ~ . .
+TXT
+},
+
+
 headband:  # todo/check: female version smaller?
   { colors: ['1A6ED5', 'FFFFFF'],
     offset: [7,7],
@@ -133,6 +290,7 @@ attributes.each do |key,attribute|
   punk = Image.new( 24, 24 )
   punk.compose!( img, *attribute[:offset] )
   punk.save( "./tmp/#{key}.png" )
+  punk.zoom(8).save( "./tmp/#{key}x8.png" )
 end
 
 
