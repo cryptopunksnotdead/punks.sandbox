0ab9350ec5a85b39cc100fdeb9c23548569b6711
Gerald Bauer <gerald.bauer@gmail.com>
2021-08-14 21:39:00 +0200

    up

--- blocks/README.md
+++ blocks/README.md
@@ -53,43 +53,95 @@ Eyes
 
 Lips
 
+![](i/woman-lips-party_pink.png)
 ![](i/woman-lips-passion_red.png)
 ![](i/woman-lips-burgundy.png)
-![](i/woman-lips-purple.png)
-![](i/woman-lips-party_pink.png)
 ![](i/woman-lips-flashy_blue.png)
-![](i/woman-lips-gold.png)
+![](i/woman-lips-purple.png)
 ![](i/woman-lips-space.png)
+![](i/woman-lips-gold.png)
 
 4x
 
+![](i/woman-lips-party_pink4x.png)
 ![](i/woman-lips-passion_red4x.png)
 ![](i/woman-lips-burgundy4x.png)
-![](i/woman-lips-purple4x.png)
-![](i/woman-lips-party_pink4x.png)
 ![](i/woman-lips-flashy_blue4x.png)
-![](i/woman-lips-gold4x.png)
+![](i/woman-lips-purple4x.png)
 ![](i/woman-lips-space4x.png)
+![](i/woman-lips-gold4x.png)
 
 
 
 Lips (Smiley)
 
-
+![](i/woman-lips-smiley-party_pink.png)
 ![](i/woman-lips-smiley-passion_red.png)
 ![](i/woman-lips-smiley-burgundy.png)
-![](i/woman-lips-smiley-purple.png)
-![](i/woman-lips-smiley-party_pink.png)
 ![](i/woman-lips-smiley-flashy_blue.png)
-![](i/woman-lips-smiley-gold.png)
+![](i/woman-lips-smiley-purple.png)
 ![](i/woman-lips-smiley-space.png)
+![](i/woman-lips-smiley-gold.png)
 
 4x
 
+![](i/woman-lips-smiley-party_pink4x.png)
 ![](i/woman-lips-smiley-passion_red4x.png)
 ![](i/woman-lips-smiley-burgundy4x.png)
-![](i/woman-lips-smiley-purple4x.png)
-![](i/woman-lips-smiley-party_pink4x.png)
 ![](i/woman-lips-smiley-flashy_blue4x.png)
-![](i/woman-lips-smiley-gold4x.png)
+![](i/woman-lips-smiley-purple4x.png)
 ![](i/woman-lips-smiley-space4x.png)
+![](i/woman-lips-smiley-gold4x.png)
+
+
+Samples
+
+![](i/woman-i_a.png)
+![](i/woman-ii_a.png)
+![](i/woman-iii_a.png)
+![](i/woman-iiii_a.png)
+![](i/woman-v_a.png)
+![](i/woman-vi_a.png)
+
+4x
+![](i/woman-i_ax4.png)
+![](i/woman-ii_ax4.png)
+![](i/woman-iii_ax4.png)
+![](i/woman-iiii_ax4.png)
+![](i/woman-v_ax4.png)
+![](i/woman-vi_ax4.png)
+
+
+![](i/woman-i_b.png)
+![](i/woman-ii_b.png)
+![](i/woman-iii_b.png)
+![](i/woman-iiii_b.png)
+![](i/woman-v_b.png)
+![](i/woman-vi_b.png)
+
+4x
+![](i/woman-i_bx4.png)
+![](i/woman-ii_bx4.png)
+![](i/woman-iii_bx4.png)
+![](i/woman-iiii_bx4.png)
+![](i/woman-v_bx4.png)
+![](i/woman-vi_bx4.png)
+
+
+
+![](i/woman-i_c.png)
+![](i/woman-ii_c.png)
+![](i/woman-iii_c.png)
+![](i/woman-iiii_c.png)
+![](i/woman-v_c.png)
+![](i/woman-vi_c.png)
+
+4x
+![](i/woman-i_cx4.png)
+![](i/woman-ii_cx4.png)
+![](i/woman-iii_cx4.png)
+![](i/woman-iiii_cx4.png)
+![](i/woman-v_cx4.png)
+![](i/woman-vi_cx4.png)
+
+
--- blocks/women.rb
+++ blocks/women.rb
@@ -24,9 +24,9 @@ design = <<TXT
 . . . . . . . . @ x x x x x x @ . . . . . . . .
 . . . . . . . @ x x x x x x x x @ . . . . . . .
 . . . . . . . @ x x x x x x x x @ . . . . . . .
-. . . . . . . @ x @ @ x x x @ @ @ . . . . . . .
-. . . . . . @ x x x x x x x x x @ . . . . . . .
-. . . . . . @ x x o @ x x x o @ @ . . . . . . .
+. . . . . . . @ x x x x x x x x @ . . . . . . .
+. . . . . . @ x x ^ ^ x x x ^ ^ @ . . . . . . .
+. . . . . . @ x x @ o x x x @ o @ . . . . . . .
 . . . . . . @ @ x x x x x x x x @ . . . . . . .
 . . . . . . . @ x x x x x x x x @ . . . . . . .
 . . . . . . . @ x x x x @ x x x @ . . . . . . .
@@ -55,10 +55,20 @@ skin_tones = {
   night_goddess: '4B228D',
 }
 
+
 pp skin_tones
 
+
 skin_tones.each do |key,color|
-  woman = Image.parse( design, colors: [BLACK, color, WHITE])
+  h,s,l  = Color.to_hsl( Color.parse(color))
+  color2 = Color.from_hsl( h, [s-0.1,0.0].max, [l-0.20,0.0].max )
+
+  # print "%20s  " % key
+  # print "#{Color.format( Color.parse(color) )}"
+  # print "\n"
+
+
+  woman = Image.parse( design, colors: [BLACK, color, WHITE, color2])
   woman.save( "./i/woman-#{key}.png")
   woman.zoom(4).save( "./i/woman-#{key}4x.png" )
   woman.zoom(8).save( "./i/woman-#{key}8x.png" )
@@ -66,20 +76,20 @@ end
 
 
 eyes_design = <<TXT
-x @ . . . x @
+ @ x . . . @ x
 TXT
 
 eyes_colors = {
-  blue:   '259FAC',  ## blue_eye_roll
-  brown:  '682B15',  ## brown_to_the_right
-  green: '8EA226',   ## green_eye_roll
-  purple: '792CC7',  ## purple_straight
+  blue:   '259FAC',   ## blue_eye_roll
+  brown:  '682B15',   ## brown_to_the_right
+  green:  '8EA226',   ## green_eye_roll
+  purple: '792CC7',   ## purple_straight
 }
 
 eyes_colors.each do |key,color|
   eyes = Image.parse( eyes_design, colors: [color, WHITE])
   woman = Image.new( 24, 24 )
-  woman.compose!( eyes, 9, 3 )
+  woman.compose!( eyes, 9, 13 )
   woman.save( "./i/woman-eyes-#{key}.png")
   woman.zoom(4).save( "./i/woman-eyes-#{key}4x.png" )
   woman.zoom(8).save( "./i/woman-eyes-#{key}8x.png" )
@@ -143,6 +153,258 @@ lips_colors.each do |key,color|
   woman.zoom(8).save( "./i/woman-lips-smiley-#{key}8x.png" )
 end
 
+
+
+
+####
+## test some pics with hair
+
+sidehair_design = <<TXT
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . . @ @ @ @ . . . . . . . . . .
+. . . . . . . . @ @ @ @ @ @ @ @ . . . . . . . .
+. . . . . . . @ @ @ @ @ @ @ @ @ @ . . . . . . .
+. . . . . . . @ @ @ @ @ @ @ @ @ @ . . . . . . .
+. . . . . . . @ @ . . . . @ . @ @ @ . . . . . .
+. . . . . . @ @ . . . . . . . . @ @ . . . . . .
+. . . . . . @ @ . . . . . . . . @ @ . . . . . .
+. . . . . . @ . . . . . . . . . @ @ . . . . . .
+. . . . . . @ . . . . . . . . . @ @ . . . . . .
+. . . . . . . . . . . . . . . . @ @ @ . . . . .
+. . . . . . . . . . . . . . . . @ @ @ . . . . .
+. . . . . . . . . . . . . . . . @ @ @ . . . . .
+. . . . . . . . . . . . . . . . @ @ @ . . . . .
+. . . . . . . . . . . . . . . . @ @ @ . . . . .
+. . . . . . . . . . . . . . . @ @ @ @ . . . . .
+. . . . . . . . . . . . . . @ @ @ @ @ . . . . .
+. . . . . . . . . . . . . . @ @ @ . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . .
+TXT
+
+wildhair_design = <<TXT
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . . @ @ @ @ . @ . . . . . . . .
+. . . . . . . @ . @ @ @ @ @ @ . . . . . . . . .
+. . . . . . . @ @ @ @ @ @ @ @ @ . @ . . . . . .
+. . . . . @ @ @ @ @ @ @ @ @ @ @ @ . @ . . . . .
+. . . . . . @ @ @ @ . @ @ @ @ @ @ @ @ . . . . .
+. . . . . @ @ @ @ . @ . @ . . @ @ @ . . . . . .
+. . . . @ . @ @ @ . . . @ . . . @ @ @ . . . . .
+. . . . . @ @ @ . . . . @ . . . . @ . @ . . . .
+. . . . @ . @ . . . . . . @ . . . @ @ . . . . .
+. . . @ @ @ @ . . . . . . . . . . @ @ . . . . .
+. . . . . @ @ . . . . . . . . . . @ @ @ . . . .
+. . . . . @ @ . . . . . . . . . . @ . . . . . .
+. . . . @ . . . . . . . . . . . . . @ . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . .
+TXT
+
+
+pigtails_design = <<TXT
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . @ @ @ @ @ @ . . . . . . . . .
+. . . . @ @ . . @ @ @ @ @ @ @ @ . . @ @ . . . .
+. . . @ @ @ @ x @ @ @ @ @ @ @ @ x @ @ @ @ . . .
+. . @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ . .
+. . @ @ @ @ . @ @ @ . . . . . @ @ . @ @ @ @ . .
+. . @ @ @ . . @ @ . . . . . . . @ . . @ @ @ . .
+. . @ @ @ . . @ . . . . . . . . . . . @ @ @ . .
+. . . @ . . . . . . . . . . . . . . . . @ . . .
+. . . . . . . . . . . . . . . . . . . . . . . .
+TXT
+
+bob_design = <<TXT
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . @ @ @ @ @ @ . . . . . . . . .
+. . . . . . . @ @ @ @ @ @ @ @ @ . . . . . . . .
+. . . . . . . @ @ @ @ @ @ @ @ @ @ . . . . . . .
+. . . . . . @ @ @ @ @ @ @ @ . @ @ @ . . . . . .
+. . . . . . @ @ . @ @ @ @ . . . @ @ . . . . . .
+. . . . . . @ @ . . . @ . . . . @ @ . . . . . .
+. . . . . @ @ @ . . . . . . . . @ @ . . . . . .
+. . . . . @ @ . . . . . . . . . @ @ . . . . . .
+. . . . . @ @ . . . . . . . . . @ @ . . . . . .
+. . . . . @ @ @ . . . . . . . . @ @ . . . . . .
+. . . . . @ @ @ . . . . . . . . @ @ @ . . . . .
+. . . . . @ @ @ . . . . . . . . @ @ @ . . . . .
+. . . . @ @ @ @ @ . . . . . . . @ @ @ @ . . . .
+. . . . . . @ @ @ @ . . . . . @ @ . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . .
+TXT
+
+ORANGE     = 'e65700'
+GREEN      = '28b143'
+BLONDE     = 'fff68e'
+WHITESMOKE = 'f5f5f5'
+
+
+sidehair_black      = Image.parse( sidehair_design, colors: [BLACK] )
+sidehair_blonde     = Image.parse( sidehair_design, colors: [BLONDE] )
+sidehair_whitesmoke = Image.parse( sidehair_design, colors: [WHITESMOKE] )
+sidehair_orange     = Image.parse( sidehair_design, colors: [ORANGE] )
+sidehair_green      = Image.parse( sidehair_design, colors: [GREEN] )
+
+
+pigtails_black      = Image.parse( pigtails_design, colors: [BLACK, 'ffd926'] )
+pigtails_blonde     = Image.parse( pigtails_design, colors: [BLONDE, 'ffd926'] )
+pigtails_whitesmoke = Image.parse( pigtails_design, colors: [WHITESMOKE, 'ffd926'] )
+pigtails_orange     = Image.parse( pigtails_design, colors: [ORANGE, 'ffd926'] )
+pigtails_green      = Image.parse( pigtails_design, colors: [GREEN, 'ffd926'] )
+pigtails_fuchsia    = Image.parse( pigtails_design, colors: ['870057', 'ffd926'] )
+pigtails_turquoise  = Image.parse( pigtails_design, colors: ['06AD93', 'ffd926'] )
+
+
+bob_black      = Image.parse( bob_design, colors: [BLACK] )
+bob_blonde     = Image.parse( bob_design, colors: [BLONDE] )
+bob_whitesmoke = Image.parse( bob_design, colors: [WHITESMOKE] )
+bob_orange     = Image.parse( bob_design, colors: [ORANGE] )
+bob_green      = Image.parse( bob_design, colors: [GREEN] )
+bob_fuchsia    = Image.parse( bob_design, colors: ['870057'] )
+bob_turquoise  = Image.parse( bob_design, colors: ['06AD93'] )
+
+
+wildhair_blonde = Image.parse( wildhair_design, colors: [BLONDE] )
+
+
+woman = Image.read( "./i/woman-light_warm_olive.png" )
+woman.compose!( bob_blonde )
+woman.save( "./i/woman_i_a.png" )
+woman.zoom(4).save( "./i/woman_i_ax4.png" )
+
+woman = Image.read( "./i/woman-light_warm_olive.png" )
+woman.compose!( bob_blonde )
+woman.compose!( Image.read( "./i/woman-eyes-blue.png") )
+woman.compose!( Image.read( "./i/woman-lips-party_pink.png") )
+woman.save( "./i/woman_i_b.png" )
+woman.zoom(4).save( "./i/woman_i_bx4.png" )
+
+woman = Image.read( "./i/woman-light_warm_olive.png" )
+woman.compose!( pigtails_blonde )
+woman.compose!( Image.read( "./i/woman-eyes-purple.png") )
+woman.compose!( Image.read( "./i/woman-lips-smile-party_pink.png") )
+woman.save( "./i/woman_i_c.png" )
+woman.zoom(4).save( "./i/woman_i_cx4.png" )
+
+
+
+woman = Image.read( "./i/woman-light_warm_olive.png" )
+woman.compose!( bob_whitesmoke )
+woman.save( "./i/woman_ii_a.png" )
+woman.zoom(4).save( "./i/woman_ii_ax4.png" )
+
+woman = Image.read( "./i/woman-light_warm_olive.png" )
+woman.compose!( bob_whitesmoke )
+woman.compose!( Image.read( "./i/woman-eyes-blue.png") )
+woman.compose!( Image.read( "./i/woman-lips-flashy_blue.png") )
+woman.save( "./i/woman_ii_b.png" )
+woman.zoom(4).save( "./i/woman_ii_bx4.png" )
+
+woman = Image.read( "./i/woman-light_warm_olive.png" )
+woman.compose!( pigtails_whitesmoke )
+woman.compose!( Image.read( "./i/woman-eyes-purple.png") )
+woman.compose!( Image.read( "./i/woman-lips-smile-flashy_blue.png") )
+woman.save( "./i/woman_ii_c.png" )
+woman.zoom(4).save( "./i/woman_ii_cx4.png" )
+
+
+
+
+woman = Image.read( "./i/woman-golden.png" )
+woman.compose!( bob_fuchsia )
+woman.save( "./i/woman_iii_a.png" )
+woman.zoom(4).save( "./i/woman_iii_ax4.png" )
+
+woman = Image.read( "./i/woman-golden.png" )
+woman.compose!( bob_fuchsia )
+woman.compose!( Image.read( "./i/woman-eyes-blue.png") )
+woman.compose!( Image.read( "./i/woman-lips-purple.png") )
+woman.save( "./i/woman_iii_b.png" )
+woman.zoom(4).save( "./i/woman_iii_bx4.png" )
+
+woman = Image.read( "./i/woman-golden.png" )
+woman.compose!( pigtails_fuchsia )
+woman.compose!( Image.read( "./i/woman-eyes-purple.png") )
+woman.compose!( Image.read( "./i/woman-lips-smile-purple.png") )
+woman.save( "./i/woman_iii_c.png" )
+woman.zoom(4).save( "./i/woman_iii_cx4.png" )
+
+
+woman = Image.read( "./i/woman-golden.png" )
+woman.compose!( bob_turquoise )
+woman.save( "./i/woman_iiii_a.png" )
+woman.zoom(4).save( "./i/woman_iiii_ax4.png" )
+
+woman = Image.read( "./i/woman-golden.png" )
+woman.compose!( bob_turquoise )
+woman.compose!( Image.read( "./i/woman-eyes-blue.png") )
+woman.compose!( Image.read( "./i/woman-lips-space.png") )
+woman.save( "./i/woman_iiii_b.png" )
+woman.zoom(4).save( "./i/woman_iiii_bx4.png" )
+
+woman = Image.read( "./i/woman-golden.png" )
+woman.compose!( pigtails_turquoise )
+woman.compose!( Image.read( "./i/woman-eyes-purple.png") )
+woman.compose!( Image.read( "./i/woman-lips-smile-space.png") )
+woman.save( "./i/woman_iiii_c.png" )
+woman.zoom(4).save( "./i/woman_iiii_cx4.png" )
+
+
+
+woman = Image.read( "./i/woman-night_goddess.png" )  ## check: fix typo
+woman.compose!( bob_black )
+woman.save( "./i/woman_v_a.png" )
+woman.zoom(4).save( "./i/woman_v_ax4.png" )
+
+woman = Image.read( "./i/woman-night_goddess.png" )  ## check: fix typo
+woman.compose!( bob_black )
+woman.compose!( Image.read( "./i/woman-eyes-green.png") )
+woman.compose!( Image.read( "./i/woman-lips-burgundy.png") )
+woman.save( "./i/woman_v_b.png" )
+woman.zoom(4).save( "./i/woman_v_bx4.png" )
+
+woman = Image.read( "./i/woman-night_goddess.png" )  ## check: fix typo
+woman.compose!( pigtails_black )
+woman.compose!( Image.read( "./i/woman-eyes-green.png") )
+woman.compose!( Image.read( "./i/woman-lips-smile-burgundy.png") )
+woman.save( "./i/woman_v_c.png" )
+woman.zoom(4).save( "./i/woman_v_cx4.png" )
+
+
+woman = Image.read( "./i/woman-night_goddess.png" )  ## check: fix typo
+woman.compose!( bob_whitesmoke )
+woman.save( "./i/woman_vi_a.png" )
+woman.zoom(4).save( "./i/woman_vi_ax4.png" )
+
+woman = Image.read( "./i/woman-night_goddess.png" )  ## check: fix typo
+woman.compose!( bob_whitesmoke )
+woman.compose!( Image.read( "./i/woman-eyes-brown.png") )
+woman.compose!( Image.read( "./i/woman-lips-burgundy.png") )
+woman.save( "./i/woman_vi_b.png" )
+woman.zoom(4).save( "./i/woman_vi_bx4.png" )
+
+woman = Image.read( "./i/woman-night_goddess.png" )  ## check: fix typo
+woman.compose!( pigtails_whitesmoke )
+woman.compose!( Image.read( "./i/woman-eyes-brown.png") )
+woman.compose!( Image.read( "./i/woman-lips-smile-burgundy.png") )
+woman.save( "./i/woman_vi_c.png" )
+woman.zoom(4).save( "./i/woman_vi_cx4.png" )
 
 
 puts "bye"
