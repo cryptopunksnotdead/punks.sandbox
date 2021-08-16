6f7a049a8f2fbce98e97062c8173c2efb6c30b57
Gerald Bauer <gerald.bauer@gmail.com>
2021-08-16 15:02:45 +0200

    up blocks

--- blocks/natural.rb
+++ blocks/natural.rb
@@ -7,6 +7,9 @@ $LOAD_PATH.unshift( "../cryptopunks/lib" )
 require 'cryptopunks'
 
 
+
+outdir = './i'    ## use ./tmp - why? why not?
+
 alien_design = <<TXT
 . . . . . . . . . . . . . . . . . . . . . . . .
 . . . . . . . . . . . . . . . . . . . . . . . .
@@ -34,10 +37,238 @@ alien_design = <<TXT
 . . . . . . ^ ^ ^ ^ ^ . . . . . . . . . . . . .
 TXT
 
+punk = Image.parse( alien_design, colors: ALIEN_COLORS )
+punk.save( "#{outdir}/alien-male-natural.png")
+punk.zoom(4).save( "#{outdir}/alien-male-naturual4x.png" )
 
-punk = Image.parse( alien_design, colors: ['000000', '75bdbd', '9be0e0', 'c8fbfb'])
-punk.save( "./tmp/alien-male-natural.png")
-punk.zoom(4).save( "./tmp/alien-male-naturual4x.png" )
+
+zombie_design =<<TXT
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . o o o o o o o . . . . . . . . .
+. . . . . . . o o o o o o o o o . . . . . . . .
+. . . . . . o o o ~ o o o o o o o . . . . . . .
+. . . . . . o o ~ o o o o o o o o . . . . . . .
+. . . . . . o o o o o o o o o o o . . . . . . .
+. . . . . . o o o o o o o o o o o . . . . . . .
+. . . . . . o o o x x o o o x x o . . . . . . .
+. . . . . o o o o ^ @ o o o ^ @ o . . . . . . .
+. . . . . o o o o x o o o o x o o . . . . . . .
+. . . . . o o o o o o o o o o o o . . . . . . .
+. . . . . . o o o o o o @ @ o o o . . . . . . .
+. . . . . . o o o o o o o o o o o . . . . . . .
+. . . . . . o o o o o o o o o o o . . . . . . .
+. . . . . . o o o o o @ @ @ o o o . . . . . . .
+. . . . . . o o o o o x o o o o o . . . . . . .
+. . . . . . o o o o o o o o o o . . . . . . . .
+. . . . . . o o o o o o o o o . . . . . . . . .
+. . . . . . o o o o o . . . . . . . . . . . . .
+. . . . . . o o o o o . . . . . . . . . . . . .
+TXT
+
+punk = Image.parse( zombie_design, colors: ZOMBIE_COLORS )
+punk.save( "#{outdir}/zombie-male-natural.png")
+punk.zoom(4).save( "#{outdir}/zombie-male-naturual4x.png" )
+
+
+ape_design =<<TXT
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . x x x x x x x . . . . . . . . .
+. . . . . . . x x x x x x x x x . . . . . . . .
+. . . . . . x x x x x x x x x x x . . . . . . .
+. . . . . . x x x x x x x x x x x . . . . . . .
+. . . . . . x x x @ @ @ @ @ @ x x . . . . . . .
+. . . . . . x x ^ ^ ^ ^ ^ ^ ^ ^ x . . . . . . .
+. . . . . . x x ^ o o ^ ^ ^ o o x . . . . . . .
+. . . . . x x x ^ @ ~ ^ ^ ^ @ ~ x . . . . . . .
+. . . . . x x x ^ ^ ^ ^ ^ ^ ^ ^ x . . . . . . .
+. . . . . x x x ^ ^ ^ ^ ^ ^ ^ ^ x . . . . . . .
+. . . . . . x x x ^ ^ @ ^ @ ^ ^ x . . . . . . .
+. . . . . . x x x x ^ ^ ^ ^ ^ x x . . . . . . .
+. . . . . . x x x ^ ^ ^ ^ ^ ^ ^ x . . . . . . .
+. . . . . . x x x ^ @ @ @ @ @ ^ x . . . . . . .
+. . . . . . x x x ^ ^ ^ ^ ^ ^ ^ x . . . . . . .
+. . . . . . x x x x ^ ^ ^ ^ ^ x . . . . . . . .
+. . . . . . x x x x x x x x x . . . . . . . . .
+. . . . . . x x x x x . . . . . . . . . . . . .
+. . . . . . x x x x x . . . . . . . . . . . . .
+TXT
+
+punk = Image.parse( ape_design, colors: APE_COLORS )
+punk.save( "#{outdir}/ape-male-natural.png")
+punk.zoom(4).save( "#{outdir}/ape-male-naturual4x.png" )
+
+ape_zombie_design =<<TXT
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . x x x x x x x . . . . . . . . .
+. . . . . . . x x x x x x x x x . . . . . . . .
+. . . . . . x x x ~ x x x x x x x . . . . . . .
+. . . . . . x x ~ x x x x x x x x . . . . . . .
+. . . . . . x x x @ @ @ @ @ @ x x . . . . . . .
+. . . . . . x x ^ ^ ^ ^ ^ ^ ^ ^ x . . . . . . .
+. . . . . . x x ^ o o ^ ^ ^ o o x . . . . . . .
+. . . . . x x x ^ % @ ^ ^ ^ % @ x . . . . . . .
+. . . . . x x x ^ o ^ ^ ^ ^ o ^ x . . . . . . .
+. . . . . x x x ^ ^ ^ ^ ^ ^ ^ ^ x . . . . . . .
+. . . . . . x x x ^ ^ @ ^ @ ^ ^ x . . . . . . .
+. . . . . . x x x x ^ ^ ^ ^ ^ x x . . . . . . .
+. . . . . . x x x ^ ^ ^ ^ ^ ^ ^ x . . . . . . .
+. . . . . . x x x ^ @ @ @ @ @ ^ x . . . . . . .
+. . . . . . x x x ^ o ^ ^ ^ ^ ^ x . . . . . . .
+. . . . . . x x x x ^ ^ ^ ^ ^ x . . . . . . . .
+. . . . . . x x x x x x x x x . . . . . . . . .
+. . . . . . x x x x x . . . . . . . . . . . . .
+. . . . . . x x x x x . . . . . . . . . . . . .
+TXT
+punk = Image.parse( ape_zombie_design, colors: ['000000', '7DA269', '5E7253', 'A1C18F', '91B080', 'FF0000'] )
+punk.save( "#{outdir}/ape-zombie-male-natural.png")
+punk.zoom(4).save( "#{outdir}/ape-zombie-male-naturual4x.png" )
+
+
+ape_alien_design =<<TXT
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . x x x x x x x . . . . . . . . .
+. . . . . . . x x x x x x x x x . . . . . . . .
+. . . . . . x x x ~ x x x x x x x . . . . . . .
+. . . . . . x x ~ x x x x x x x x . . . . . . .
+. . . . . . x x x @ @ @ @ @ @ x x . . . . . . .
+. . . . . . x x ~ ~ ~ ~ ~ ~ ~ ~ x . . . . . . .
+. . . . . x x x ~ o @ ~ ~ ~ o @ x . . . . . . .
+. . . . x x ^ x ~ @ ^ ~ ~ ~ @ ^ x . . . . . . .
+. . . . . x x x ~ ~ ~ ~ ~ ~ ~ ~ x . . . . . . .
+. . . . . x x x ~ ~ ~ ~ ~ ~ ~ ~ x . . . . . . .
+. . . . . . x x x ~ ~ ^ ~ ^ ~ ~ x . . . . . . .
+. . . . . . x x x x ~ ~ ~ ~ ~ x x . . . . . . .
+. . . . . . x x x ~ ~ ~ ~ ~ ~ ~ x . . . . . . .
+. . . . . . x x x ~ @ @ @ @ @ ~ x . . . . . . .
+. . . . . . x x x ~ ~ ~ ~ ~ ~ ~ x . . . . . . .
+. . . . . . x x x x ~ ~ ~ ~ ~ x . . . . . . . .
+. . . . . . x x x x x x x x x . . . . . . . . .
+. . . . . . x x x x x . . . . . . . . . . . . .
+. . . . . . x x x x x . . . . . . . . . . . . .
+TXT
+punk = Image.parse( ape_alien_design, colors: ['000000', 'C8FBFB', '75BDBD', '9BE0E0', 'F3FFFF'] )
+punk.save( "#{outdir}/ape-alien-male-natural.png")
+punk.zoom(4).save( "#{outdir}/ape-alien-male-naturual4x.png" )
+
+skeleton_design =<<TXT
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . x x x x x x x . . . . . . . . .
+. . . . . . . x x x x x x x x x . . . . . . . .
+. . . . . . x x x x x x x x x x x . . . . . . .
+. . . . . . x x x x x x x x x x x . . . . . . .
+. . . . . . x x x x x x x x x x x . . . . . . .
+. . . . . . x x x x x x x x x x x . . . . . . .
+. . . . . . x x x @ @ x x x @ @ x . . . . . . .
+. . . . . x x x x @ @ x x x @ @ x . . . . . . .
+. . . . . x x x x x x x x x x x x . . . . . . .
+. . . . . x x x x x x x x x x x x . . . . . . .
+. . . . . . x x x x x x @ @ x x x . . . . . . .
+. . . . . . x x x x x x x x x x x . . . . . . .
+. . . . . . x x x x @ x @ x @ x x . . . . . . .
+. . . . . . x x x x @ @ @ @ @ x x . . . . . . .
+. . . . . . x x x x @ x @ x @ x x . . . . . . .
+. . . . . . x x x x x x x x x x . . . . . . . .
+. . . . . . x x x x x x x x x . . . . . . . . .
+. . . . . . x x x x x . . . . . . . . . . . . .
+. . . . . . x x x x x . . . . . . . . . . . . .
+TXT
+punk = Image.parse( skeleton_design, colors: SKELETON_COLORS )
+punk.save( "#{outdir}/skeleton-male-natural.png")
+punk.zoom(4).save( "#{outdir}/skeleton-male-naturual4x.png" )
+
+demon_design =<<TXT
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . ^ ^ . . . . . ^ ^ . . . . . . . .
+. . . . . . . ^ ^ ^ . . . ^ ^ ^ . . . . . . . .
+. . . . . . . ^ ^ ^ ^ ^ ^ ^ ^ ^ . . . . . . . .
+. . . . . . . ^ ^ ^ ^ ^ ^ ^ ^ ^ . . . . . . . .
+. . . . . . ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ . . . . . . .
+. . . . . . ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ . . . . . . .
+. . . . . . ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ . . . . . . .
+. . . . . . ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ . . . . . . .
+. . . . . . ^ ^ ^ x x ^ ^ ^ x x ^ . . . . . . .
+. . . . . ^ ^ ^ ^ @ o ^ ^ ^ @ o ^ . . . . . . .
+. . . . . ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ . . . . . . .
+. . . . . ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ . . . . . . .
+. . . . . . ^ ^ ^ ^ ^ ^ @ @ ^ ^ ^ . . . . . . .
+. . . . . . ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ . . . . . . .
+. . . . . . ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ . . . . . . .
+. . . . . . ^ ^ ^ ^ ^ @ @ @ ^ ^ ^ . . . . . . .
+. . . . . . ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ . . . . . . .
+. . . . . . ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ . . . . . . . .
+. . . . . . ^ ^ ^ ^ ^ ^ ^ ^ ^ . . . . . . . . .
+. . . . . . ^ ^ ^ ^ ^ . . . . . . . . . . . . .
+. . . . . . ^ ^ ^ ^ ^ . . . . . . . . . . . . .
+TXT
+punk = Image.parse( demon_design, colors: DEMON_COLORS )
+punk.save( "#{outdir}/demon-male-natural.png")
+punk.zoom(4).save( "#{outdir}/demon-male-naturual4x.png" )
+
+
+
+
+human_male_design =<<TXT
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . ^ ^ ^ ^ ^ ^ ^ . . . . . . . . .
+. . . . . . . ^ ^ ^ ^ ^ ^ ^ ^ ^ . . . . . . . .
+. . . . . . ^ ^ ^ ~ ^ ^ ^ ^ ^ ^ ^ . . . . . . .
+. . . . . . ^ ^ ~ ^ ^ ^ ^ ^ ^ ^ ^ . . . . . . .
+. . . . . . ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ . . . . . . .
+. . . . . . ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ . . . . . . .
+. . . . . . ^ ^ ^ x x ^ ^ ^ x x ^ . . . . . . .
+. . . . . ^ ^ ^ ^ @ o ^ ^ ^ @ o ^ . . . . . . .
+. . . . . ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ . . . . . . .
+. . . . . ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ . . . . . . .
+. . . . . . ^ ^ ^ ^ ^ ^ @ @ ^ ^ ^ . . . . . . .
+. . . . . . ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ . . . . . . .
+. . . . . . ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ . . . . . . .
+. . . . . . ^ ^ ^ ^ ^ @ @ @ ^ ^ ^ . . . . . . .
+. . . . . . ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ . . . . . . .
+. . . . . . ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ . . . . . . . .
+. . . . . . ^ ^ ^ ^ ^ ^ ^ ^ . . . . . . . . . .
+. . . . . . ^ ^ ^ ^ ^ . . . . . . . . . . . . .
+. . . . . . ^ ^ ^ ^ ^ . . . . . . . . . . . . .
+TXT
+
+males = {
+  lighter: HUMAN_COLORS_LIGHTER,
+  light:   HUMAN_COLORS_LIGHT,
+  dark:    HUMAN_COLORS_DARK,
+  darker:  HUMAN_COLORS_DARKER
+}
+
+males.each do |skintone, colors|
+  punk = Image.parse( human_male_design, colors: colors )
+  punk.save( "#{outdir}/human-male-#{skintone}-natural.png")
+  punk.zoom(4).save( "#{outdir}/human-male-#{skintone}-naturual4x.png" )
+end
+
 
 
 puts "bye"
