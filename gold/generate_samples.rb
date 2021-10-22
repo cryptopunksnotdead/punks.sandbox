19158bb069e148d0ac8782088c91af21791960a3
Gerald Bauer <gerald.bauer@gmail.com>
2021-10-22 14:09:03 +0200

    up

---
+++ gold/generate_samples.rb
@@ -0,0 +1,127 @@
+###############################
+# to run use:
+#    $  ruby ./generate_samples.rb
+
+
+$LOAD_PATH.unshift( "../cryptopunks/lib" )
+require 'cryptopunks'
+
+
+names = ['Gold 1', 'Gold 2', 'Gold 3']
+
+
+composite = ImageComposite.new( 6, 3 )
+
+names.each do |name|
+
+  composite << Punks::Image.generate( "Skeleton #{name}" )
+  composite << Punks::Image.generate( "Skeleton Female #{name}" )
+
+  composite << Punks::Image.generate( "Skeleton #{name}",
+                                      'Big Hair Blonde' )
+  composite << Punks::Image.generate( "Skeleton Female #{name}",
+                                      'Blonde Bob',
+                                      'Flowers' )
+
+  composite << Punks::Image.generate( "Skeleton #{name}",
+                                      'Sun Hat',
+                                      '3D Glasses',
+                                      'Pipe' )
+  composite << Punks::Image.generate( "Skeleton Female #{name}",
+                                      'Sun Hat',
+                                      '3D Glasses',
+                                      'Pipe' )
+end
+
+composite.save( "./tmp/skeletons.png" )
+composite.zoom(2).save( "./tmp/skeletons@2x.png" )
+composite.zoom(4).save( "./tmp/skeletons@4x.png" )
+
+
+
+
+composite = ImageComposite.new( 6, 3 )
+
+names.each do |name|
+  composite << Punks::Image.generate( "Demon #{name}" )
+  composite << Punks::Image.generate( "Demon Female #{name}" )
+
+  composite << Punks::Image.generate( "Demon #{name}",
+                                      'Heart Shades' )
+  composite << Punks::Image.generate( "Demon Female #{name}",
+                                      'Heart Shades' )
+
+  composite << Punks::Image.generate( "Demon #{name}",
+                                      'Sombrero',
+                                      '3D Glasses',
+                                      'Pipe' )
+  composite << Punks::Image.generate( "Demon Female #{name}",
+                                      'Sombrero',
+                                      '3D Glasses',
+                                      'Pipe' )
+end
+
+composite.save( "./tmp/demons.png" )
+composite.zoom(2).save( "./tmp/demons@2x.png" )
+composite.zoom(4).save( "./tmp/demons@4x.png" )
+
+
+
+composite = ImageComposite.new( 6, 3 )
+
+names.each do |name|
+  composite << Punks::Image.generate( "Alien #{name}" )
+  composite << Punks::Image.generate( "Alien Female #{name}" )
+
+  composite << Punks::Image.generate( "Alien #{name}",
+                                      'Birthday Hat' )
+  composite << Punks::Image.generate( "Alien Female #{name}",
+                                      'Flowers' )
+
+  composite << Punks::Image.generate( "Alien #{name}",
+                                      'Cap Forward',
+                                      'Small Shades',
+                                      'Pipe' )
+  composite << Punks::Image.generate( "Alien Female #{name}",
+                                      'Cap Forward',
+                                      'Small Shades',
+                                      'Pipe' )
+end
+
+composite.save( "./tmp/aliens.png" )
+composite.zoom(2).save( "./tmp/aliens@2x.png" )
+composite.zoom(4).save( "./tmp/aliens@4x.png" )
+
+
+
+
+composite = ImageComposite.new( 6, 3 )
+
+names.each do |name|
+  composite << Punks::Image.generate( "Zombie #{name}" )
+  composite << Punks::Image.generate( "Zombie Female #{name}" )
+
+  composite << Punks::Image.generate( "Zombie #{name}",
+                                      'Crazy Hair' )
+  composite << Punks::Image.generate( "Zombie Female #{name}",
+                                      'Crazy Hair' )
+
+  composite << Punks::Image.generate( "Zombie #{name}",
+                                      'Wild Hair',
+                                      '3D Glasses',
+                                      'Pipe' )
+  composite << Punks::Image.generate( "Zombie Female #{name}",
+                                      'Wild Hair',
+                                      '3D Glasses',
+                                      'Pipe' )
+end
+
+composite.save( "./tmp/zombies.png" )
+composite.zoom(2).save( "./tmp/zombies@2x.png" )
+composite.zoom(4).save( "./tmp/zombies@4x.png" )
+
+
+
+
+
+puts "bye"
--- gold/gold.rb
+++ gold/gold.rb
@@ -48,16 +48,14 @@ colors = {
 base_m = Punks::Sheet.find_by( name: 'Skeleton' )
 base_f = Punks::Sheet.find_by( name: 'Skeleton Female' )
 
+base_m.zoom.save( "./tmp/skeleton-male.png" )
+base_m.zoom( 8 ).save( "./tmp/skeleton-male@8x.png" )
+
+base_f.zoom.save( "./tmp/skeleton-female.png" )
+base_f.zoom( 8 ).save( "./tmp/skeleton-female@8x.png" )
+
 
 dump_colors( base_m )
-
-
-base_m.zoom.save( "./tmp/skeleton_male.png" )
-base_m.zoom( 8 ).save( "./tmp/skeleton_male@8x.png" )
-
-base_f.zoom.save( "./tmp/skeleton_female.png" )
-base_f.zoom( 8 ).save( "./tmp/skeleton_female@8x.png" )
-
 
 def derive_skeleton_color_map( color )
 # 122 pixels #e0e0e0 / rgb(224 224 224) - hsl(  0°   0%  88%)           - 8-BIT GRAYSCALE #224
@@ -65,90 +63,44 @@ def derive_skeleton_color_map( color )
 # 385 pixels #000000 / rgb(  0   0   0) - hsl(  0°   0%   0%) - α(  0%) - TRANSPARENT
 #  69 pixels #000000 / rgb(  0   0   0) - hsl(  0°   0%   0%)           - BLACK
 
-base = color
+  base = color
 
-color_map = {
-  '#e0e0e0' =>  base,
-}
+  color_map = {
+    '#e0e0e0' =>  base,
+  }
 
-color_map
+  color_map
 end
 
 
-composite = ImageComposite.new( 6, 3 )
 
 colors.each do |name, color|
   color_map = derive_skeleton_color_map( color )
   punk_m = base_m.change_colors( color_map )
 
-  composite << punk_m
-
-  punk_m.save( "./tmp/skeleton_male_#{name}.png" )
-  punk_m.zoom(4).save( "./tmp/skeleton_male_#{name}@4x.png" )
+  punk_m.save( "./tmp/skeleton-male_#{name}.png" )
+  punk_m.zoom(4).save( "./tmp/skeleton-male_#{name}@4x.png" )
 
 
   punk_f = base_f.change_colors( color_map )
 
-  composite << punk_f
-
-  punk_f.save( "./tmp/skeleton_female_#{name}.png" )
-  punk_f.zoom(4).save( "./tmp/skeleton_female_#{name}@4x.png" )
-
-
-  punk = Image.new( 24, 24 )
-  punk.compose!( punk_m )
-  punk.compose!( Punks::Sheet.find_by( name: 'Big Hair Blonde', gender: 'm' ))
-  composite << punk
-
-  punk = Image.new( 24, 24 )
-  punk.compose!( punk_f )
-  punk.compose!( Punks::Sheet.find_by( name: 'Blonde Bob', gender: 'f' ))
-  punk.compose!( Punks::Sheet.find_by( name: 'Flowers', gender: 'f' ))
-  composite << punk
-
-
-  punk = Image.new( 24, 24 )
-  punk.compose!( punk_m )
-  punk.compose!( Punks::Sheet.find_by( name: 'Sun Hat', gender: 'm' ))
-  punk.compose!( Punks::Sheet.find_by( name: '3D Glasses', gender: 'm' ))
-  punk.compose!( Punks::Sheet.find_by( name: 'Pipe', gender: 'm' ))
-  composite << punk
-
-  punk = Image.new( 24, 24 )
-  punk.compose!( punk_f )
-  punk.compose!( Punks::Sheet.find_by( name: 'Sun Hat', gender: 'f' ))
-  punk.compose!( Punks::Sheet.find_by( name: '3D Glasses', gender: 'f' ))
-  punk.compose!( Punks::Sheet.find_by( name: 'Pipe', gender: 'f' ))
-  composite << punk
+  punk_f.save( "./tmp/skeleton-female_#{name}.png" )
+  punk_f.zoom(4).save( "./tmp/skeleton-female_#{name}@4x.png" )
 end
 
 
-composite.save( "./tmp/skeletons.png" )
-composite.zoom(2).save( "./tmp/skeletons@2x.png" )
-composite.zoom(4).save( "./tmp/skeletons@4x.png" )
-
-
-
-
-
-
-
-
-__END__
 
 base_m = Punks::Sheet.find_by( name: 'Demon' )
 base_f = Punks::Sheet.find_by( name: 'Demon Female' )
 
+base_m.zoom.save( "./tmp/demon-male.png" )
+base_m.zoom( 8 ).save( "./tmp/demon-male@8x.png" )
+
+base_f.zoom.save( "./tmp/demon-female.png" )
+base_f.zoom( 8 ).save( "./tmp/demon-female@8x.png" )
+
 
 dump_colors( base_m )
-
-
-base_m.zoom.save( "./tmp/demon_male.png" )
-base_m.zoom( 8 ).save( "./tmp/demon_male@8x.png" )
-
-base_f.zoom.save( "./tmp/demon_female.png" )
-base_f.zoom( 8 ).save( "./tmp/demon_female@8x.png" )
-
 
 def derive_demon_color_map( color )
 # 134 pixels #850008 / rgb(133   0   8) - hsl(356° 100%  26%)  - base
@@ -188,64 +140,19 @@ color_map
 end
 
 
-composite = ImageComposite.new( 6, 3 )
-
 colors.each do |name, color|
   color_map = derive_demon_color_map( color )
   punk_m = base_m.change_colors( color_map )
 
-  composite << punk_m
-
-  punk_m.save( "./tmp/demon_male_#{name}.png" )
-  punk_m.zoom(4).save( "./tmp/demon_male_#{name}@4x.png" )
+  punk_m.save( "./tmp/demon-male_#{name}.png" )
+  punk_m.zoom(4).save( "./tmp/demon-male_#{name}@4x.png" )
 
 
   punk_f = base_f.change_colors( color_map )
 
-  composite << punk_f
-
-  punk_f.save( "./tmp/demon_female_#{name}.png" )
-  punk_f.zoom(4).save( "./tmp/demon_female_#{name}@4x.png" )
-
-
-  punk = Image.new( 24, 24 )
-  punk.compose!( punk_m )
-  punk.compose!( Punks::Sheet.find_by( name: 'Heart Shades', gender: 'm' ))
-  composite << punk
-
-  punk = Image.new( 24, 24 )
-  punk.compose!( punk_f )
-  punk.compose!( Punks::Sheet.find_by( name: 'Heart Shades', gender: 'f' ))
-  composite << punk
-
-
-  punk = Image.new( 24, 24 )
-  punk.compose!( punk_m )
-  punk.compose!( Punks::Sheet.find_by( name: 'Sombrero', gender: 'm' ))
-  punk.compose!( Punks::Sheet.find_by( name: '3D Glasses', gender: 'm' ))
-  punk.compose!( Punks::Sheet.find_by( name: 'Pipe', gender: 'm' ))
-  composite << punk
-
-  punk = Image.new( 24, 24 )
-  punk.compose!( punk_f )
-  punk.compose!( Punks::Sheet.find_by( name: 'Sombrero', gender: 'f' ))
-  punk.compose!( Punks::Sheet.find_by( name: '3D Glasses', gender: 'f' ))
-  punk.compose!( Punks::Sheet.find_by( name: 'Pipe', gender: 'f' ))
-  composite << punk
+  punk_f.save( "./tmp/demon-female_#{name}.png" )
+  punk_f.zoom(4).save( "./tmp/demon-female_#{name}@4x.png" )
 end
-
-
-composite.save( "./tmp/demons.png" )
-composite.zoom(2).save( "./tmp/demons@2x.png" )
-composite.zoom(4).save( "./tmp/demons@4x.png" )
-
-
-
-
-
-
-
-__END__
 
 
 
@@ -253,16 +160,14 @@ __END__
 base_m = Punks::Sheet.find_by( name: 'Alien' )
 base_f = Punks::Sheet.find_by( name: 'Alien Female' )
 
+base_m.zoom.save( "./tmp/alien-male.png" )
+base_m.zoom( 8 ).save( "./tmp/alien-male@8x.png" )
+
+base_f.zoom.save( "./tmp/alien-female.png" )
+base_f.zoom( 8 ).save( "./tmp/alien-female@8x.png" )
+
 
 dump_colors( base_m )
-
-
-base_m.zoom.save( "./tmp/alien_male.png" )
-base_m.zoom( 8 ).save( "./tmp/alien_male@8x.png" )
-
-base_f.zoom.save( "./tmp/alien_female.png" )
-base_f.zoom( 8 ).save( "./tmp/alien_female@8x.png" )
-
 
 def derive_alien_color_map( color )
 
@@ -309,81 +214,36 @@ color_map
 end
 
 
-composite = ImageComposite.new( 6, 3 )
-
 colors.each do |name, color|
   color_map = derive_alien_color_map( color )
   punk_m = base_m.change_colors( color_map )
 
-  composite << punk_m
-
-  punk_m.save( "./tmp/alien_male_#{name}.png" )
-  punk_m.zoom(4).save( "./tmp/alien_male_#{name}@4x.png" )
+  punk_m.save( "./tmp/alien-male_#{name}.png" )
+  punk_m.zoom(4).save( "./tmp/alien-male_#{name}@4x.png" )
 
 
   punk_f = base_f.change_colors( color_map )
 
-  composite << punk_f
-
-  punk_f.save( "./tmp/alien_female_#{name}.png" )
-  punk_f.zoom(4).save( "./tmp/alien_female_#{name}@4x.png" )
-
-
-  punk = Image.new( 24, 24 )
-  punk.compose!( punk_m )
-  punk.compose!( Punks::Sheet.find_by( name: 'Birthday Hat', gender: 'm' ))
-  composite << punk
-
-  punk = Image.new( 24, 24 )
-  punk.compose!( punk_f )
-  punk.compose!( Punks::Sheet.find_by( name: 'Flowers', gender: 'f' ))
-  composite << punk
-
-
-  punk = Image.new( 24, 24 )
-  punk.compose!( punk_m )
-  punk.compose!( Punks::Sheet.find_by( name: 'Cap Forward', gender: 'm' ))
-  punk.compose!( Punks::Sheet.find_by( name: 'Small Shades', gender: 'm' ))
-  punk.compose!( Punks::Sheet.find_by( name: 'Pipe', gender: 'm' ))
-  composite << punk
-
-  punk = Image.new( 24, 24 )
-  punk.compose!( punk_f )
-  punk.compose!( Punks::Sheet.find_by( name: 'Cap Forward', gender: 'f' ))
-  punk.compose!( Punks::Sheet.find_by( name: 'Small Shades', gender: 'f' ))
-  punk.compose!( Punks::Sheet.find_by( name: 'Pipe', gender: 'f' ))
-  composite << punk
+  punk_f.save( "./tmp/alien-female_#{name}.png" )
+  punk_f.zoom(4).save( "./tmp/alien-female_#{name}@4x.png" )
 end
 
 
-composite.save( "./tmp/aliens.png" )
-composite.zoom(2).save( "./tmp/aliens@2x.png" )
-composite.zoom(4).save( "./tmp/aliens@4x.png" )
 
-
-
-
-
-
-
-
-
-__END__
 
 
 base_m = Punks::Sheet.find_by( name: 'Zombie' )
 base_f = Punks::Sheet.find_by( name: 'Zombie Female' )
 
 
+base_m.zoom.save( "./tmp/zombie-male.png" )
+base_m.zoom( 8 ).save( "./tmp/zombie-male@8x.png" )
+
+base_f.zoom.save( "./tmp/zombie-female.png" )
+base_f.zoom( 8 ).save( "./tmp/zombie-female@8x.png" )
+
+
 dump_colors( base_m )
-
-
-base_m.zoom.save( "./tmp/zombie_male.png" )
-base_m.zoom( 8 ).save( "./tmp/zombie_male@8x.png" )
-
-base_f.zoom.save( "./tmp/zombie_female.png" )
-base_f.zoom( 8 ).save( "./tmp/zombie_female@8x.png" )
-
 
 def derive_zombie_color_map( color )
 
@@ -422,58 +282,19 @@ def derive_zombie_color_map( color )
 end
 
 
-composite = ImageComposite.new( 6, 3 )
-
 colors.each do |name, color|
   color_map = derive_zombie_color_map( color )
   zombie_m = base_m.change_colors( color_map )
 
-  composite << zombie_m
-
-  zombie_m.save( "./tmp/zombie_male_#{name}.png" )
-  zombie_m.zoom(4).save( "./tmp/zombie_male_#{name}@4x.png" )
+  zombie_m.save( "./tmp/zombie-male_#{name}.png" )
+  zombie_m.zoom(4).save( "./tmp/zombie-male_#{name}@4x.png" )
 
 
   zombie_f = base_f.change_colors( color_map )
 
-  composite << zombie_f
-
-  zombie_f.save( "./tmp/zombie_female_#{name}.png" )
-  zombie_f.zoom(4).save( "./tmp/zombie_female_#{name}@4x.png" )
-
-
-  zombie = Image.new( 24, 24 )
-  zombie.compose!( zombie_m )
-  zombie.compose!( Punks::Sheet.find_by( name: 'Crazy Hair', gender: 'm' ))
-  composite << zombie
-
-  zombie = Image.new( 24, 24 )
-  zombie.compose!( zombie_f )
-  zombie.compose!( Punks::Sheet.find_by( name: 'Crazy Hair', gender: 'f' ))
-  composite << zombie
-
-
-  zombie = Image.new( 24, 24 )
-  zombie.compose!( zombie_m )
-  zombie.compose!( Punks::Sheet.find_by( name: 'Wild Hair', gender: 'm' ))
-  zombie.compose!( Punks::Sheet.find_by( name: '3D Glasses', gender: 'm' ))
-  zombie.compose!( Punks::Sheet.find_by( name: 'Pipe', gender: 'm' ))
-  composite << zombie
-
-  zombie = Image.new( 24, 24 )
-  zombie.compose!( zombie_f )
-  zombie.compose!( Punks::Sheet.find_by( name: 'Wild Hair', gender: 'f' ))
-  zombie.compose!( Punks::Sheet.find_by( name: '3D Glasses', gender: 'f' ))
-  zombie.compose!( Punks::Sheet.find_by( name: 'Pipe', gender: 'f' ))
-  composite << zombie
+  zombie_f.save( "./tmp/zombie-female_#{name}.png" )
+  zombie_f.zoom(4).save( "./tmp/zombie-female_#{name}@4x.png" )
 end
-
-
-composite.save( "./tmp/zombies.png" )
-composite.zoom(2).save( "./tmp/zombies@2x.png" )
-composite.zoom(4).save( "./tmp/zombies@4x.png" )
-
-
 
 
 
