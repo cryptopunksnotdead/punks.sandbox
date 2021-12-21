dec163e5e6a346f785e9762d4f87bdd6d45998a8
Gerald Bauer <gerald.bauer@gmail.com>
2021-12-21 13:13:29 +0100

    up

---
+++ frame/README.md
@@ -0,0 +1,77 @@
+# Frame Your Punks (24x24px) - Free Golden Pixel Art Frame (36x36px) & More
+
+
+Let's frame punks (24x24px). Let's try the free golden pixel art frame (36x36).
+
+![](i/frame24x24.png)
+
+4x <br> ![](i/frame24x24@4x.png)
+
+
+
+Let's try the frameless punks
+\#2890 ![](i/punk-2890.png),
+\#8219 ![](i/punk-8219.png),
+\#10180 ![](i/punk-10180.png),
+\#10190 ![](i/punk-10190.png).
+
+
+Here we go:
+
+![](i/framedpunk-2890.png)
+![](i/framedpunk-8219.png)
+![](i/framedpunk-10180.png)
+![](i/framedpunk-10190.png)
+
+4x <br>
+![](i/framedpunk-2890@4x.png)
+![](i/framedpunk-8219@4x.png)
+![](i/framedpunk-10180@4x.png)
+![](i/framedpunk-10190@4x.png)
+
+8x <br>
+![](i/framedpunk-2890@8x.png)
+![](i/framedpunk-8219@8x.png)
+![](i/framedpunk-10180@8x.png)
+![](i/framedpunk-10190@8x.png)
+
+
+
+
+Let's try the frameless "oil painting" punks
+![](i/mona_lisa.png),
+![](i/girl_with_a_pearl_earring.png),
+![](i/van_gogh_self-portrait.png),
+![](i/the_scream.png).
+
+Here we go:
+
+![](i/framed-mona_lisa.png)
+![](i/framed-girl_with_a_pearl_earring.png)
+![](i/framed-van_gogh_self-portrait.png)
+![](i/framed-the_scream.png)
+
+4x <br>
+
+![](i/framed-mona_lisa@4x.png)
+![](i/framed-girl_with_a_pearl_earring@4x.png)
+![](i/framed-van_gogh_self-portrait@4x.png)
+![](i/framed-the_scream@4x.png)
+
+8x <br>
+
+![](i/framed-mona_lisa@8x.png)
+![](i/framed-girl_with_a_pearl_earring@8x.png)
+![](i/framed-van_gogh_self-portrait@8x.png)
+![](i/framed-the_scream@8x.png)
+
+
+
+That's it for now.
+
+
+## Questions? Comments?
+
+Post them on the [CryptoPunksDev reddit](https://old.reddit.com/r/CryptoPunksDev). Thanks.
+
+
---
+++ frame/frame.rb
@@ -0,0 +1,73 @@
+
+
+require 'pixelart'
+
+
+
+frame = Image.read( "./i/frame24x24.png" )
+
+
+
+frame.zoom( 4 ).save( "./i/frame24x24@4x.png" )
+
+
+composite = ImageComposite.new( 2, 2, width: 36, height: 36 )  # 2x2 = 4
+
+
+[2890, 8219, 10180, 10190].each do |id|
+
+  frameless = Image.read( "./i/punk-#{id}.png" )
+
+  punk = Image.new( 36, 36 )
+  punk.compose!( frame )
+  punk.compose!( Image.new( 24,24, '#638596' ), 6, 6 )  ## add non-trasparent / opaque background first
+  punk.compose!( frameless, 6, 6 )
+
+  punk.save( "./i/framedpunk-#{id}.png" )
+  punk.zoom(2).save( "./i/framedpunk-#{id}@2x.png" )
+  punk.zoom(4).save( "./i/framedpunk-#{id}@4x.png" )
+  punk.zoom(8).save( "./i/framedpunk-#{id}@8x.png" )
+
+  composite << punk
+end
+
+composite.save( "./i/framedpunks.png" )
+composite.zoom(2).save( "./i/framedpunks@2.png" )
+composite.zoom(4).save( "./i/framedpunks@4x.png" )
+composite.zoom(8).save( "./i/framedpunks@8x.png" )
+
+
+
+
+composite = ImageComposite.new( 2, 2, width: 36, height: 36 )  # 2x2 = 4
+
+[
+  'mona_lisa',
+  'girl_with_a_pearl_earring',
+  'van_gogh_self-portrait',
+  'the_scream',
+].each do |slug|
+
+  frameless = Image.read( "./i/#{slug}.png" )
+
+  punk = Image.new( 36, 36 )
+  punk.compose!( frame )
+  punk.compose!( frameless, 6, 6 )
+
+  punk.save( "./i/framed-#{slug}.png" )
+  punk.zoom(2).save( "./i/framed-#{slug}@2x.png" )
+  punk.zoom(4).save( "./i/framed-#{slug}@4x.png" )
+  punk.zoom(8).save( "./i/framed-#{slug}@8x.png" )
+
+  composite << punk
+end
+
+
+composite.save( "./i/framedpunks_ii.png" )
+composite.zoom(2).save( "./i/framedpunks_ii@2.png" )
+composite.zoom(4).save( "./i/framedpunks_ii@4x.png" )
+composite.zoom(8).save( "./i/framedpunks_ii@8x.png" )
+
+
+
+puts "bye"
