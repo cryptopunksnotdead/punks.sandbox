fd2337a8f83bbd3f2614b9328ee02f8e17f98188
Gerald Bauer <gerald.bauer@gmail.com>
2022-07-10 14:21:39 +0200

    up

--- saudis/saudis.rb
+++ saudis/saudis.rb
@@ -3,6 +3,16 @@ require 'pixelart'
 
 
 
+def slugify( name )
+  ## e.g. turn Red Shemagh & Agal  => red_shemagh_agal
+  ##           Darker 2            => darker_2
+  name = name.downcase.gsub( /[^a-zA-Z0-9 _-]/, '' )
+  name = name.gsub( /[ ]+/, '_' )
+  name
+end
+
+
+
 specs = Csv.parse( <<TXT )
   Light 1, Normal Brown Beard & Mustache,  Red Shemagh & Agal, Regular Pixel Shades
   Light 1, Messy White Beard,   Brown Shemagh & Agal, Big Purple Shades
@@ -14,14 +24,6 @@ TXT
 
 pp specs
 
-
-def slugify( name )
-  ## e.g. turn Red Shemagh & Agal  => red_shemagh_agal
-  ##           Darker 2            => darker_2
-  name = name.downcase.gsub( /[^a-zA-Z0-9 _-]/, '' )
-  name = name.gsub( /[ ]+/, '_' )
-  name
-end
 
 
 
@@ -44,6 +46,48 @@ end
 saudis.save( "./tmp/saudis.png" )
 saudis.zoom(4).save( "./tmp/saudis@4x.png" )
 saudis.zoom(8).save( "./tmp/saudis@8x.png" )
+
+
+###
+#  saudis vol. 2 - try all 10 shemags (headwear)
+
+
+specs = Csv.parse( <<TXT )
+  Light 1, Red Shemagh
+  Light 1, Red Shemagh & Agal
+  Light 2, Red Shemagh & Crown
+  Light 2, White Shemagh
+  Medium 1, White Shemagh & Agal, Natural Cigarette
+  Medium 2, White Shemagh & Gold Agal
+  Dark 1, White Shemagh & Stylish Gold Agal, Stylish Mustache
+  Dark 2, White Shemagh & Crown
+  Darker 1, Brown Shemagh & Agal
+  Darker 2, Brown Shemagh & Crown,  Bubble Gum
+TXT
+
+pp specs
+
+
+
+saudis = ImageComposite.new( 5, 2, background: '#006C35' )
+
+specs.each_with_index do |attributes,i|
+   saudi = Image.new( 24, 24 )
+   attributes.each do |attribute|
+      slug = slugify( attribute )
+      saudi.compose!( Image.read( "./attributes/#{slug}.png" ))
+   end
+
+   saudi.save( "./tmp/saudi#{i}-vol2.png" )
+   saudi.zoom(8).save( "./tmp/saudi#{i}-vol2@8x.png" )
+
+   saudis << saudi.mirror    ## phree the phunks
+end
+
+
+saudis.save( "./tmp/saudis-vol2.png" )
+saudis.zoom(4).save( "./tmp/saudis-vol2@4x.png" )
+saudis.zoom(8).save( "./tmp/saudis-vol2@8x.png" )
 
 
 
