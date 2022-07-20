1a93ad1e8b734c224c8a0c2669d59d4be5f0740c
Gerald Bauer <gerald.bauer@gmail.com>
2022-07-20 12:56:16 +0200

    up

---
+++ flags/europeans.rb
@@ -0,0 +1,99 @@
+######
+#  generate europeans
+#   $ ruby ./europeans.rb
+
+require 'punks'
+require 'backgrounds'
+
+
+flags = %w[
+  europe
+
+  austria
+  czechia
+  germany
+  hungary
+  liechtenstein
+  poland
+  slovakia
+  slovenia
+  switzerland
+
+  belgium
+  france
+  luxembourg
+  netherlands
+
+  denmark
+  finland
+  estonia
+  iceland
+  latvia
+  lithuania
+  norway
+  sweden
+
+  great_britain
+  england
+  ireland
+  scotland
+  wales
+
+
+  albania
+  croatia
+  north_macedonia
+  romania
+  bulgaria
+  georgia
+  ukraine
+
+
+  spain
+  portugal
+
+  italy
+  vatican_city
+  malta
+  greece
+  cyprus
+  turkey
+]
+
+
+
+puts "  #{flags.size} flag(s)"
+#=> 43 flags(s)
+#  to make it a round 42 remove  russia for now
+
+
+series = [
+  ['Female 3', 'Wild Blonde', 'Big Shades', 'Smile', 'Mole'],
+  ['Ape', 'Bandana', 'Bubble Gum'],
+  [],
+]
+
+punks = ImageComposite.new( 14, 3*series.size )
+
+
+
+series.each do |attributes|
+  flags.each do |name|
+   flag = Image.read( "./#{name}.png" )
+
+   if attributes.empty?
+      punks << flag
+   else
+     punk = Punk::Image.generate( *attributes )
+     punks << punk.background( flag )
+   end
+  end
+end
+
+punks.save( "./tmp/european_flags.png" )
+punks.zoom(2).save( "./tmp/european_flags@2x.png" )
+punks.zoom(4).save( "./tmp/european_flags@4x.png" )
+
+
+
+puts "bye"
