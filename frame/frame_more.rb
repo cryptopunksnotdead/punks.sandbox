0d38ddd77b509829bbd3143a6a9f2e515a76923f
Gerald Bauer <gerald.bauer@gmail.com>
2022-07-27 13:12:37 +0200

    up

--- frame/frame_more.rb
+++ frame/frame_more.rb
@@ -4,6 +4,42 @@ require 'cryptopunks'
 
 
 
+frame = Image.read( "./i/frame24x24_(4).png" ) ## "wood" frame in 38x36
+
+frame.zoom( 4 ).save( "./i/frame24x24_(4)@4x.png" )
+
+
+composite = ImageComposite.new( 2, 2, width: 38+4, height: 36+4 )  # 2x2 = 4
+
+
+[2890, 8219, 10180, 10190].each do |id|
+
+  frameless = Image.read( "./i/punk-#{id}.png" )
+
+  punk = Image.new( 38, 36 )
+  punk.compose!( Image.new( 24,24, '#ffbf00' ), 7, 6 )  ## add non-trasparent / opaque background first
+  punk.compose!( frame )
+  punk.compose!( frameless, 7, 6 )
+
+  punk.save( "./i/framedpunk_(4)-#{id}.png" )
+  punk.zoom(2).save( "./i/framedpunk_(4)-#{id}@2x.png" )
+  punk.zoom(4).save( "./i/framedpunk_(4)-#{id}@4x.png" )
+  punk.zoom(8).save( "./i/framedpunk_(4)-#{id}@8x.png" )
+
+  ## add 4px (2p on each side transparent padding/border)
+  punk_with_padding = Image.new( 38+4, 36+4 )
+  punk_with_padding.compose!( punk, 2, 2)
+
+  composite << punk_with_padding
+end
+
+
+composite.save( "./i/framedpunks_(4).png" )
+composite.zoom(2).save( "./i/framedpunks_(4)@2.png" )
+composite.zoom(4).save( "./i/framedpunks_(4)@4x.png" )
+composite.zoom(8).save( "./i/framedpunks_(4)@8x.png" )
+
+
 
 
 
