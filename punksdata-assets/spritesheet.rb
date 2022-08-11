950ae4c301e6de1a07c9957beb1fdde2eca9038f
Gerald Bauer <gerald.bauer@gmail.com>
2022-08-11 21:31:45 +0200

    up

--- punksdata-assets/spritesheet.rb
+++ punksdata-assets/spritesheet.rb
@@ -9,7 +9,7 @@ require_relative '../punksdata/punksdata'
 
 
 
-assets = ImageComposite.new( 10, 14 )   # use grid 10x14 = 140 attributes / tiles
+assets = ImageComposite.new( 10, 14+2 )   # use grid 10x14 = 140 attributes / tiles
 
 
 
@@ -48,6 +48,72 @@ ASSETS_ENCODED.each_with_index do |rec,i|
            names[1..-1].join( ' | '),
           ]
 end
+
+
+##
+## add more attributs
+
+more = Csv.parse( <<TXT )
+### more archetypes
+
+zombie-female.png,  Zombie Female,    f
+ape-female.png,     Ape Female,       f
+alien-female.png,   Alien Female,     f
+
+### more hair attributes
+f/peakspike.png,   Peak Spike, f
+f/purplehair.png,    Purple Hair, f
+f/shavedhead.png,  Shaved Head,   f
+f/vampirehair.png,  Vampire Hair, f
+
+m/blondebob.png,     Blonde Bob, m
+m/blondeshort.png,   Blonde Short, m
+m/darkhair.png,      Dark Hair, m
+m/halfshaved.png,    Half Shaved, m
+m/orangeside.png,    Orange Side, m
+m/pigtails.png,      Pigtails, m
+m/pinkwithhat.png,   Pink With Hat, m
+m/redmohawk.png,     Red Mohawk, m
+m/straighthair.png,       Straight Hair, m
+m/straighthairblonde.png, Straight Hair Blonde, m
+m/straighthairdark.png,   Straight Hair Dark, m
+m/wildblonde.png,         Wild Blonde, m
+m/wildwhitehair.png,      Wild White Hair, m
+
+TXT
+
+pp more
+
+
+more.each_with_index do |rec,i|
+
+  path = rec[0]
+  img = Image.read( "./more/#{path}" )
+
+  assets << img
+
+  ## auto-generate metadata record
+  name = rec[1]
+  names = [name]
+
+  if i <= 2    ## all archetype (0..2) - 3 all-together
+    gender, size =  ['f', 's']
+    category = 'Archetype'
+  else
+    qualifier = rec[2]
+    gender, size = qualifier == 'f' ? ['f', 's'] : ['m', 'l']
+    category = '?'    ## to be done later
+  end
+
+  meta << [meta.size,
+           name,
+           gender,
+           size,
+           category,
+           names[1..-1].join( ' | '),
+          ]
+end
+
 
 
 assets.save( "tmp/spritesheet/punks_2017-24x24.png" )
